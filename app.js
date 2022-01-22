4d61abf9f69de2fd38cb10a0f81f091afb135898
VishrutMKrishna <91896854+VishrutMKrishna@users.noreply.github.com>
2022-01-22 16:03:24 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const internetButn = document.getElementById("internet-butn");
-internetButn.onclick =()=>{
-    location.href="./internet.html";
-}
-
-const scamButn = document.getElementById("scam-butn");
-scamButn.onclick =()=>{
-    location.href="./scam.html";
-}
