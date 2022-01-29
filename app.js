c9f95435c6978df22152fc7c9573a98c3ee7852f
VishrutMKrishna <91896854+VishrutMKrishna@users.noreply.github.com>
2022-01-29 17:19:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const interetButn = document.getElementById("internet-butn");
+const interetButn = document.getElementById("internetbutn");
 internetButn.onclick =()=>{
     location.href="./internet.html";
 }
 
-const scamButn = document.getElementById("scam-butn");
+const scamButn = document.getElementById("scambutn");
 scamButn.onclick =()=>{
     location.href="./scam.html";
 }
 
-const safetyButn = document.getElementById("safety-btn");
+const safetyButn = document.getElementById("safetybtn");
 safetyButn.onclick =()=>{
     location.href="./scam.html";
 }
